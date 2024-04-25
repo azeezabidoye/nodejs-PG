3a420aaa74e42fec97dff220a6f0ae417cf15dff
azeezabidoye <azeezabidoye@yahoo.com>
2024-04-25 15:01:35 +0100

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+// Function for generating ID for a new Note
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+// Create new Note
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+// Homepage
+app.get("/", (request, response) => {
+  response.send("<h1>Hello Express!</h1>");
+});
+
+// Get all Notes
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+// Get Note by ID
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+  console.log(note);
+  response.json(note);
+});
+
+// Delete Note by ID
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
